8a8a843d6ad41ce9f99dbdbcffa189a3205f848e
Kyle Pfromer <kpfromer2@gmail.com>
2020-09-25 10:17:11 -0600

    refactor: client signals to file

--- configuration/init.lua
+++ configuration/init.lua
@@ -1,9 +1,7 @@
 local awful = require('awful')
-local gears = require("gears")
-local wibox = require("wibox")
 local config = require('configuration.config')
 
-require('configuration.client.rules')
+require('configuration.client')
 
 -- Global Keybindings
 _G.root.keys(require("configuration.keys.global"))
@@ -42,66 +40,6 @@ function autostart()
 end
 -- TODO:
 -- autostart()
-
--- If a window wants titlebars (based on window rules) then give to them (see config.rules)
--- Handle Titlebars (min/max/close bar on every window)
--- https://awesomewm.org/doc/api/classes/client.html#request::titlebars
-_G.client.connect_signal(
-    "request::titlebars",
-    function(c)
-        -- buttons for the titlebar
-        local buttons =
-            gears.table.join(
-            -- Click on titlebar
-            awful.button(
-                {},
-                1,
-                function()
-                    c:emit_signal("request::activate", "titlebar", {raise = true})
-                    awful.mouse.client.move(c)
-                end
-            ),
-            -- Right click titlebar
-            awful.button(
-                {},
-                3,
-                function()
-                    c:emit_signal("request::activate", "titlebar", {raise = true})
-                    awful.mouse.client.resize(c)
-                end
-            )
-        )
-
-        awful.titlebar(c):setup {
-            {
-                -- Left
-                awful.titlebar.widget.iconwidget(c),
-                buttons = buttons,
-                layout = wibox.layout.fixed.horizontal
-            },
-            {
-                -- Middle
-                {
-                    -- Title
-                    align = "center",
-                    widget = awful.titlebar.widget.titlewidget(c)
-                },
-                buttons = buttons,
-                layout = wibox.layout.flex.horizontal
-            },
-            {
-                -- Right
-                awful.titlebar.widget.floatingbutton(c),
-                awful.titlebar.widget.maximizedbutton(c),
-                awful.titlebar.widget.stickybutton(c),
-                awful.titlebar.widget.ontopbutton(c),
-                awful.titlebar.widget.closebutton(c),
-                layout = wibox.layout.fixed.horizontal()
-            },
-            layout = wibox.layout.align.horizontal
-        }
-    end
-)
 
 -- TODO: comment
 _G.tag.connect_signal('property::layout', function(t)
--- rc.lua
+++ rc.lua
@@ -263,58 +263,6 @@ root.buttons(
     )
 )
 
--- {{{ Signals
--- Signal function to execute when a new client appears.
-client.connect_signal(
-    "manage",
-    function(c)
-        -- Set the windows at the slave,
-        -- i.e. put it at the end of others instead of setting it master.
-        -- if not awesome.startup then awful.client.setslave(c) end
-
-        if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-            -- Prevent clients from being unreachable after screen count changes.
-            awful.placement.no_offscreen(c)
-        end
-    end
-)
-
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal(
-    "mouse::enter",
-    function(c)
-        c:emit_signal("request::activate", "mouse_enter", {raise = false})
-    end
-)
-
-client.connect_signal(
-    "focus",
-    function(c)
-        c.border_color = beautiful.border_focus
-    end
-)
-client.connect_signal(
-    "unfocus",
-    function(c)
-        c.border_color = beautiful.border_normal
-    end
-)
--- }}}
-
--- Make the focused window have a glowing border
-_G.client.connect_signal(
-    "focus",
-    function(c)
-        c.border_color = beautiful.border_focus
-    end
-)
-_G.client.connect_signal(
-    "unfocus",
-    function(c)
-        c.border_color = beautiful.border_normal
-    end
-)
-
 -- Autostart Applications
 
 awful.spawn.with_shell("sh ~/.config/awesome/autostart")
