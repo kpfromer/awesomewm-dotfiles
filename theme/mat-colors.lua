f8d5a7de52f2e3f73673e0ec0075c5fba7d31959
Kyle Pfromer <kpfromer2@gmail.com>
2020-09-26 16:01:15 -0600

    feat: add glorius theme

--- theme/mat-colors.lua
+++ theme/mat-colors.lua
@@ -1,3 +1,4 @@
+-- TODO: remove
 return {
     -- Red
     red = {
--- theme/werewolf/init.lua
+++ theme/werewolf/init.lua
@@ -3,8 +3,10 @@ local mat_colors = require('theme.mat-colors')
 local theme_dir = filesystem.get_configuration_dir() .. '/theme'
 
 local theme = {}
+
 theme.icons = theme_dir .. '/icons/'
-theme.font = 'Roboto medium 10'
+theme.font = 'Inter Regular 10'
+theme.font_bold = 'Inter Bold 10'
 
 -- Colors Pallets
 
@@ -20,14 +22,44 @@ theme.primary.hue_700 = '#bd93f9'
 theme.primary.hue_800 = '#44475a'
 theme.primary.hue_900 = '#282a36'
 -- theme.primary = mat_colors.indigo
--- Accent
-theme.accent = mat_colors.orange
 
--- Background
-theme.background = mat_colors.blue_grey
+-- Colorscheme
+theme.system_black_dark = '#3D4C5F'
+theme.system_black_light = '#56687E'
 
-theme.background.hue_800 = '#44475a'
-theme.background.hue_900 = '#44475a'
+theme.system_red_dark = '#EE4F84'
+theme.system_red_light = '#F48FB1'
+
+theme.system_green_dark = '#53E2AE'
+theme.system_green_light = '#A1EFD3'
+
+theme.system_yellow_dark = '#F1FF52'
+theme.system_yellow_light = '#F1FA8C'
+
+theme.system_blue_dark = '#6498EF' 
+theme.system_blue_light = '#92B6F4'
+
+theme.system_magenta_dark = '#985EFF'
+theme.system_magenta_light = '#BD99FF'
+
+theme.system_cyan_dark = '#24D1E7'
+theme.system_cyan_light = '#87DFEB'
+
+theme.system_white_dark = '#E5E5E5'
+theme.system_white_light = '#F8F8F2'
+
+-- Accent color
+theme.accent = theme.system_blue_dark
+
+-- Background color
+theme.background = '#00000066'
+theme.background_light = '#f2f2f266'
+
+-- Transparent
+theme.transparent = '#00000000'
+
+-- Awesome icon
+theme.awesome_icon = theme.icons .. 'awesome.svg'
 
 local awesome_overrides = function(theme) end
 return {theme = theme, awesome_overrides = awesome_overrides}
