2d24f03634885704c59a1a58be16d3a4658f5513
Kyle Pfromer <kpfromer2@gmail.com>
2020-10-27 15:57:13 -0600

    chore: clean up build and package lock

--- ts/build/src/configuration/client/index.lua
+++
@@ -1,5 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-require("ts.build.src.configuration.client.rules")
-require("ts.build.src.configuration.client.signals")
-return ____exports
