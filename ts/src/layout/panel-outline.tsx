f41b2804de7b1ab75f35a6f7e8d6de07e053571d
Kyle Pfromer <kpfromer2@gmail.com>
2020-12-02 21:51:10 -0700

    fix: update paneloutline to use theme

--- ts/src/layout/panel-outline.tsx
+++ ts/src/layout/panel-outline.tsx
@@ -1,18 +1,25 @@
-import Awesome from 'awesome/jsx';
-import * as wibox from 'wibox';
-import * as gears from 'gears';
-import * as beautiful from 'beautiful';
 import {Background, Margin} from 'awesome/components/base';
+import Awesome from 'awesome/jsx';
+import * as beautiful from 'beautiful';
+import * as gears from 'gears';
+import * as wibox from 'wibox';
 import theme from '../theme/index';
 
 const dpi = beautiful.xresources.apply_dpi;
 
+interface Props {
+  backgroundColor?: string;
+  outlineColor?: string;
+}
+
 export const PanelOutline: JSX.FunctionComponent<
-  {
-    backgroundColor?: string;
-  },
+  Props,
   wibox.BaseWiboxWidget
-> = ({children, backgroundColor = theme.transparent!}) => {
+> = ({
+  children,
+  backgroundColor = theme.transparent!,
+  outlineColor = theme!.groups_title_bg,
+}) => {
   return wibox.widget(
     <Margin top={dpi(9)} bottom={dpi(9)}>
       <Background
@@ -20,7 +27,7 @@ export const PanelOutline: JSX.FunctionComponent<
         shape={(cr, w, h) => {
           gears.shape.rounded_rect(cr, w, h, dpi(12));
         }}
-        border_color={beautiful.groups_title_bg}
+        border_color={outlineColor}
         border_width={dpi(1)}
       >
         {children}
