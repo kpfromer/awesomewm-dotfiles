ec9a236cdda5c121af9814808466f7d325f9210b
Kyle Pfromer <kpfromer2@gmail.com>
2020-12-02 21:13:53 -0700

    feat: add theme to my-panel

--- ts/src/layout/my-panel.tsx
+++ ts/src/layout/my-panel.tsx
@@ -1,15 +1,16 @@
+import {Layout, Margin} from 'awesome/components/base';
 import Awesome from 'awesome/jsx';
 import * as awful from 'awful';
 import * as beautiful from 'beautiful';
-import {Layout, Margin} from 'awesome/components/base';
 import config from '../configuration/config';
+import theme from '../theme/index';
+import {Bluetooth} from './components/bluetooth';
 import {Clock} from './components/clock';
 import {LayoutStatus} from './components/layout-status';
-import {Bluetooth} from './components/bluetooth';
-import {PanelOutline} from './panel-outline';
 import {SystemTrayToggle} from './components/systray-toggle';
-import {TaskList} from './components/task-list';
 import {TagList} from './components/tag-list';
+import {TaskList} from './components/task-list';
+import {PanelOutline} from './panel-outline';
 const dpi = beautiful.xresources.apply_dpi;
 
 const {militaryTime} = config;
@@ -26,39 +27,30 @@ screen.connect_signal(
       ontop: true,
       type: 'dock',
       height: dpi(48),
-      bg: '#00000066',
+      bg: theme.background!,
       // TODO: fix beautiful variables not found (they are undefined!!)
       // bg: beautiful.background,
       fg: beautiful.fg_normal,
     });
 
-    const font = 'Inter Bold 11';
+    // logToFile(beautiful);
+    // logToFile(theme, '/tmp/awesome-3.txt');
 
-    const regular = '#00000033';
+    const font = theme.font!;
+
     const focus = '#ffffff66';
-    const selectedColor = '#1B9AAA';
 
     panel.setup(
       <Margin left={dpi(5)} right={dpi(5)}>
         <Layout align horizontal expand="none">
           <Layout fixed horizontal>
             <PanelOutline>
-              <TagList
-                screen={screen}
-                regular={regular}
-                focus={focus}
-                selectedColor={selectedColor}
-              />
+              <TagList screen={screen} focus={focus} />
             </PanelOutline>
 
             <Margin left={dpi(5)}>
               <PanelOutline>
-                <TaskList
-                  screen={screen}
-                  regular={regular}
-                  focus={focus}
-                  selectedColor={selectedColor}
-                />
+                <TaskList screen={screen} focus={focus} />
               </PanelOutline>
             </Margin>
           </Layout>
