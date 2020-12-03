cc625970fff263726aa2f34aac2bc0c93044ccb3
Kyle Pfromer <kpfromer2@gmail.com>
2020-12-02 20:53:23 -0700

    feat: add dump helper function

--- ts/src/helper/log.ts
+++ ts/src/helper/log.ts
@@ -21,3 +21,35 @@ export const log = function (
     message,
   });
 };
+
+function dump(this: void, table: object, indent = 0): string {
+  const spacing = string.rep('  ', indent);
+  let result = '';
+  for (const [key, value] of Object.entries(table)) {
+    result += spacing;
+
+    if (type(value) === 'table') {
+      result += `${key}:\n`;
+      result += dump(value, indent + 1);
+    } else {
+      result += `${key}: ${value}\n`;
+    }
+  }
+
+  return result;
+}
+
+export const dumpData = dump;
+
+export function logToFile(
+  this: void,
+  value: any,
+  location = '/tmp/awesome.txt'
+): void {
+  const text = type(value) === 'table' ? dump(value) : value;
+
+  awful.spawn.easy_async_with_shell(
+    `echo "${text.replace('"', '\\"')}" > ${location}`,
+    () => {}
+  );
+}
