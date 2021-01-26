376a934c819cd5ffe366b7371415db3864bf01d7
Kyle Pfromer <kpfromer2@gmail.com>
2021-01-26 09:42:56 -0700

    chore: clean up rc

--- ts/src/rc.ts
+++ ts/src/rc.ts
@@ -9,38 +9,43 @@ import * as awful from 'awful';
 import * as beautiful from 'beautiful';
 import * as gears from 'gears';
 import { isString } from 'helper/type-check';
+
+// ░█▀▀░█░█░█▀▀░█░░░█░░
+// ░▀▀█░█▀█░█▀▀░█░░░█░░
+// ░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀▀▀
+// https://awesomewm.org/apidoc/libraries/awful.util.html#shell
+awful.util.shell = 'sh';
+
 // ░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀░█░█░█▀▄░█▀█░▀█▀░▀█▀░█▀█░█▀█
 // ░█░░░█░█░█░█░█▀▀░░█░░█░█░█░█░█▀▄░█▀█░░█░░░█░░█░█░█░█
 // ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░░▀░░▀▀▀░▀▀▀░▀░▀
+
 import './configuration/client/index';
 import globalKeys from './configuration/keys/global';
-// import './configuration/root'
 import './configuration/tags/index';
+
 // ░█░░░█▀█░█░█░█▀█░█░█░▀█▀
 // ░█░░░█▀█░░█░░█░█░█░█░░█░
 // ░▀▀▀░▀░▀░░▀░░▀▀▀░▀▀▀░░▀░
+
 import './layout/my-panel';
-// import * as globalKeys from './configuration/keys/global';
-// root.keys(globalKeys);
-// require('configuration.client')
-// require('configuration.root')
-// require('configuration.tags')
-// _G.root.keys(require('configuration.keys.global'))
+
 // ░█▄█░█▀█░█▀▄░█░█░█░░░█▀▀░█▀▀
 // ░█░█░█░█░█░█░█░█░█░░░█▀▀░▀▀█
 // ░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀▀▀░▀▀▀
-// // Configuration/layout for system notifications (using naughty)
-// require('module.notifications')
+
 // // Handles staring up programs like picom
 import './module/auto-start';
+// // Configuration/layout for system notifications (using naughty)
 import './module/notifications';
 // // Titlebars for clients (windows)
 import './module/titlebar';
+
 // ░▀█▀░█░█░█▀▀░█▄█░█▀▀
 // ░░█░░█▀█░█▀▀░█░█░█▀▀
-// ░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀/*  */
+// ░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀
+
 import theme from './theme/index';
-// TODO: require('awful.autofocus')
 
 // ░█▀▀░█░█░█▀▀░█░░░█░░
 // ░▀▀█░█▀█░█▀▀░█░░░█░░
@@ -53,12 +58,10 @@ beautiful.init(theme);
 
 root.keys = globalKeys as any;
 
-// // TODO: Fix weird load up bug (lockscreen fixes this?)
+// TODO: Fix weird load up bug (lockscreen fixes this?)
 // require('module.dynamic-wallpaper')
-
 // require('module.menu')
-
-// // TODO: fix
+// TODO: fix
 // require('module.lockscreen')
 
 // ░█░█░█▀█░█░░░█░░░█▀█░█▀█░█▀█░█▀▀░█▀▄
@@ -77,7 +80,6 @@ screen.connect_signal('request::wallpaper', (screen) => {
           return gears.wallpaper.set(wallpaper);
         default:
           // If beautiful.wallpaper is path/image
-
           return gears.wallpaper.maximized(wallpaper, screen);
       }
     } else {
